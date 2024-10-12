c3f37d2095105313bc381381820b3ed9414cad3e
Rui Alberto Reinehr Filho <rui.reinehr@gmail.com>
2024-10-12 10:19:30 -0300

    res

--- eloquente js prática/xadrez.js
+++ eloquente js prática/xadrez.js
@@ -1,5 +1,23 @@
-let grade = "# # # #\n # # # #\n# # # #\n # # # #\n# # # #\n # # # #\n# # # #\n # # # #\n"
-console.log(grade)
-
-
-let size = ""
+function createChessboard(size) {
+    let board = "";
+  
+    for (let row = 0; row < size; row++) {
+      for (let col = 0; col < size; col++) {
+        // Se a soma dos índices de linha e coluna for par, insira "#", caso contrário, insira espaço
+        if ((row + col) % 2 === 0) {
+          board += "#";
+        } else {
+          board += " ";
+        }
+      }
+      // Adiciona uma nova linha após cada linha ser construída
+      board += "\n";
+    }
+  
+    return board;
+  }
+  
+  // Definindo o tamanho
+  let size = 5;
+  console.log(createChessboard(size));
+  
