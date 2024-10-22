6ead11e39dee92e654300405ce69c90c8972f6a4
Rui Alberto Reinehr Filho <rui.reinehr@gmail.com>
2024-10-22 15:31:06 -0300

    res

---
+++ eloquente js prática/1-50/function2.js
@@ -0,0 +1,6 @@
+var cade = function(x) {
+    return x * x;
+}
+
+console.log (cade(7))
+
--- eloquente js prática/xadrez.js
+++ eloquente js prática/xadrez.js
@@ -2,22 +2,17 @@ function createChessboard(size) {
     let board = "";
   
     for (let row = 0; row < size; row++) {
-      for (let col = 0; col < size; col++) {
-        // Se a soma dos índices de linha e coluna for par, insira "#", caso contrário, insira espaço
-        if ((row + col) % 2 === 0) {
+      for (let col = 0; col < size; col++) {        
+        if ((row + col) % 2 == 0) {
           board += "#";
         } else {
           board += " ";
         }
-      }
-      // Adiciona uma nova linha após cada linha ser construída
+      }      
       board += "\n";
-    }
-  
+    }  
     return board;
-  }
-  
-  // Definindo o tamanho
-  let size = 5;
+  }  
+  let size = 40;
   console.log(createChessboard(size));
   
