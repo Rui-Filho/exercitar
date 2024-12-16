0d33fd7cc7321d14248d715d15869ae598f4164d
Rui Alberto Reinehr Filho <rui.reinehr@gmail.com>
2024-12-16 00:19:59 -0300

    res

--- eloquente js prática/xadrez.js
+++ eloquente js prática/xadrez.js
@@ -1,18 +1,7 @@
-function createChessboard(size) {
-    let board = "";
-  
-    for (let row = 0; row < size; row++) {
-      for (let col = 0; col < size; col++) {        
-        if ((row + col) % 2 == 0) {
-          board += "#";
-        } else {
-          board += " ";
-        }
-      }      
-      board += "\n";
-    }  
-    return board;
-  }  
-  let size = 40;
-  console.log(createChessboard(size));
-  
+var obj = {
+  name: "Alice",
+  age: 25,
+  hobbies: ["reading", "coding"]
+};
+
+console.log(obj.hobbies)
